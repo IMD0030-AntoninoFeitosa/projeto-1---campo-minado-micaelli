f4d11c7b0d211ac6ff4584eefedfe1b27b58c263
micaelli-2022 <micaelli.santos.102@ufrn.edu.br>
2022-05-03 15:28:21 -0300

    inicio matriz

--- main.cpp
+++ main.cpp
@@ -1,7 +1,92 @@
-#include<iostream>
+ #include <iostream>
+ #include<time>
 
-int main(int argc, char const *argv[])
+//enum  { beginner, intermediary, advanced };
+
+
+void iniciante (){
+	int L=10, C=10; 
+    char matriz[L][C];
+	int bombas=10;
+
+
+ srand(time(NULL))
+	for (int quantidade=0; quantidade<bombas; quantidade++){
+
+		i= rand() & L;
+		j= rand() & C;
+		matriz[i][j]=='X';
+	}
+
+
+
+
+
+
+}
+
+void intermediario (){
+	int L=15, C=15; 
+    char matriz[L][C];
+	int bombas=40;
+
+
+ srand(time(NULL))
+	for (int quantidade=0; quantidade<bombas; quantidade++){
+
+		i= rand() & L;
+		j= rand() & C;
+		matriz[i][j]=='X';
+	}
+
+
+
+}
+
+
+
+
+void avancado(){
+
+int L=15, C=30; 
+    char matriz[L][C];
+	int bombas=100;
+
+
+ srand(time(NULL))
+	for (int quantidade=0; quantidade<bombas; quantidade++){
+
+		i= rand() & L;
+		j= rand() & C;
+		matriz[i][j]=='X';
+	}
+
+
+}
+
+
+
+int main (int arg, char **argv)
 {
-	/* code */
+    
+	char action; 
+   
+
+	std:: cin >> action;  //recebe a ação que o usuario quer realizar; 
+
+	switch (action){
+       case 'b': iniciante();
+	   break;
+
+	   case 'i': intermediario();
+	   break;
+
+	   case 'a': avancado();
+	   break;
+	}
+
+	
+
+
 	return 0;
 }
